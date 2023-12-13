25cf146fc4b47b3d7b39721d05e13090b28613af
Stefhanoob <129519982+Stefhanoob@users.noreply.github.com>
2023-12-13 10:12:57 -0500

    Add files via upload

---
+++ login.js
@@ -0,0 +1,13 @@
+const username = document.getElementById('tilin69')
+const password = document.getElementById('tilin')
+const button = document.getElementById('button')
+
+button.addEventListener('click', (e) => {
+        e.preventDefault()
+        const data = {
+            username: tilin69.value,
+            password: tilin.value
+        }
+
+        console.log(data)
+    })
